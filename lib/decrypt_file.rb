2473e53b338f141169201ca5cd5066cbaa8bb7de
July Tran <julyytran@gmail.com>
2015-12-16 13:42:40 -0700

    Travis CI

--- lib/decrypt_file.rb
+++ lib/decrypt_file.rb
@@ -3,6 +3,5 @@ require_relative 'enigma.rb'
 e = Enigma.new
 e.key = ARGV[2]
 e.date = ARGV[3].to_i
-message = File.read(ARGV[0])
-File.write(ARGV[1], e.decrypt(message.strip, ARGV[2]))
+File.write(ARGV[1], e.decrypt(File.read(ARGV[0]).strip, ARGV[2]))
 puts "Created #{ARGV[1]} with the key #{e.key} and date #{e.date}"
--- lib/encrypt_file.rb
+++ lib/encrypt_file.rb
@@ -1,6 +1,5 @@
 require_relative 'enigma.rb'
 
 e = Enigma.new
-message = File.read(ARGV[0])
-File.write(ARGV[1], e.encrypt(message.strip))
+File.write(ARGV[1], e.encrypt(File.read(ARGV[0]).strip))
 puts "Created #{ARGV[1]} with the key #{e.key} and date #{e.date}"

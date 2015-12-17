fc2bf3e0775ba26d8317265326c5f8b311959671
July Tran <julyytran@gmail.com>
2015-12-16 19:34:20 -0700

    Corrected file errors

--- lib/decrypt_file.rb
+++ lib/decrypt_file.rb
@@ -1,6 +1,6 @@
 require_relative 'enigma.rb'
 
-e = Enigma.new(key, date)
+e = Enigma.new(key = ARGV[2], date = ARGV[3])
 e.key = ARGV[2]
 e.date = ARGV[3].to_i
 File.write(ARGV[1], e.decrypt(File.read(ARGV[0]).strip, ARGV[2]))
